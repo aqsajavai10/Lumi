2c5a68cb8718301b0a0b31677d28af54aa43a1b8
aqsajavai10 <145002052+aqsajavai10@users.noreply.github.com>
2025-03-16 21:00:16 +0500

    Refactored -- Unused code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
